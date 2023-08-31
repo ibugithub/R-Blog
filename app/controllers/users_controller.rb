4df5cc1283b4cd5f70cef0daac18f5374656cc8c
ibugithub <outsideworkibrahim@gmail.com>
2023-08-31 10:17:26 +0600

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
   def index
     @users = User.all
   end
@@ -9,5 +8,4 @@ class UsersController < ApplicationController
     @user = User.find(user_id)
     @posts = @user.posts
   end
-
 end
