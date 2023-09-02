6632d319c86d1b4768f5e01ef78330615768e039
ibugithub <outsideworkibrahim@gmail.com>
2023-09-02 08:47:41 +0600

    Add forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
+  resources :users do
+    resources :posts do
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create]
     end
   end
 end
