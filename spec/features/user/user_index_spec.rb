04366c6d9055b03549efe938684ff9de744cb770
gdumani <gdumani@gmail.com>
2023-09-06 09:58:39 -0600

    Add user index integration tests

--- spec/features/user/user_index_spec.rb
+++ spec/features/user/user_index_spec.rb
@@ -1,32 +1,32 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature 'User index page', type: :feature do
-#   before do
-#     @user1 = create(:user, :with_posts, photo: 'alice.jpg')
-#     @user2 = create(:user, :with_posts, photo: 'bob.jpg')
-#   end
+RSpec.feature 'User index page', type: :feature do
+  before do
+    @user1 = create(:user, :with_posts, photo: 'alice.jpg')
+    @user2 = create(:user, :with_posts, photo: 'bob.jpg')
+  end
 
-#   scenario 'I can see the username of all other users' do
-#     visit root_path
-#     expect(page).to have_content(@user1.name)
-#     expect(page).to have_content(@user2.name)
-#   end
+  scenario 'I can see the username of all other users' do
+    visit root_path
+    expect(page).to have_content(@user1.name)
+    expect(page).to have_content(@user2.name)
+  end
 
-#   scenario 'I can see the profile picture for each user' do
-#     visit root_path
-#     expect(page).to have_css("img[src*='alice.jpg']")
-#     expect(page).to have_css("img[src*='bob.jpg']")
-#   end
+  scenario 'I can see the profile picture for each user' do
+    visit root_path
+    expect(page).to have_css("img[src*='alice.jpg']")
+    expect(page).to have_css("img[src*='bob.jpg']")
+  end
 
-#   scenario 'I can see the number of posts each user has written' do
-#     visit root_path
-#     expect(page).to have_content('Number Of Posts: 3', count: 2)
-#   end
+  scenario 'I can see the number of posts each user has written' do
+    visit root_path
+    expect(page).to have_content('Number Of Posts: 3', count: 2)
+  end
 
-#   scenario 'When I click on a user, I am redirected to that user\'s show page' do
-#     visit root_path
+  scenario 'When I click on a user, I am redirected to that user\'s show page' do
+    visit root_path
 
-#     click_link @user1.name
-#     expect(current_path).to eq(user_path(@user1))
-#   end
-# end
+    click_link @user1.name
+    expect(current_path).to eq(user_path(@user1))
+  end
+end
